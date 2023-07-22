cc5244b63bac7907fdb9d3b99bc1cbae201c7367
huang-jl <1046678590@qq.com>
2023-07-22 17:05:12 +0800

    Introduce a misc drivers of pseudo_mm, which will expose some ioctl commands to userspace dev called "/dev/pseudo_mm"

---
+++ drivers/misc/pseudo_mm/pseudo_mm.c
@@ -0,0 +1,185 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * This module provides an interface to operate pseudo_mm
+ */
+
+#define pr_fmt(fmt) "pseudo_mm_driver:%s: " fmt, __func__
+
+#include <linux/device.h>
+#include <linux/fs.h>
+#include <linux/file.h>
+#include <linux/miscdevice.h>
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/pseudo_mm.h>
+
+#include "pseudo_mm_ioctl.h"
+
+static int pseudo_mm_open(struct inode *, struct file *);
+static int pseudo_mm_release(struct inode *, struct file *);
+static long pseudo_mm_unlocked_ioctl(struct file *, unsigned int,
+				     unsigned long);
+
+static const struct file_operations fops = {
+	.open = pseudo_mm_open,
+	.release = pseudo_mm_release,
+	.unlocked_ioctl = pseudo_mm_unlocked_ioctl,
+};
+
+struct miscdevice pseudo_mm_device = {
+	.minor = MISC_DYNAMIC_MINOR,
+	.name = "pseudo_mm",
+	.fops = &fops,
+};
+
+static int pseudo_mm_open(struct inode *inode, struct file *filp)
+{
+	// actually we do nothing here when open the file for now
+	return 0;
+}
+
+static int pseudo_mm_release(struct inode *inode, struct file *filp)
+{
+	// actually we do nothing here when close the file for now
+	return 0;
+}
+
+// Return 0 when succeed
+static long _pseudo_mm_add_anon(void *__user args)
+{
+	struct pseudo_mm_add_anon_param *param;
+	unsigned long err = 0;
+	unsigned long start, size;
+
+	param = kmalloc(sizeof(*param), GFP_KERNEL);
+	if (!param)
+		return -ENOMEM;
+	err = copy_from_user(param, args, sizeof(*param));
+	if (err)
+		goto free_param;
+	start = param->start;
+	if (param->end < param->start) {
+		err = -EINVAL;
+		goto free_param;
+	}
+	size = param->end - param->start;
+
+	err = pseudo_mm_add_anon_map(param->id, start, size, param->prot,
+				     param->flags);
+free_param:
+	kfree(param);
+
+	return err ? err : 0;
+}
+
+static long _pseudo_mm_fill_anon(void *__user args)
+{
+	struct pseudo_mm_fill_anon_param *param;
+	struct file *image;
+	unsigned long err = 0;
+	unsigned long start, size;
+
+	param = kmalloc(sizeof(*param), GFP_KERNEL);
+	if (!param)
+		return -ENOMEM;
+
+	err = copy_from_user(param, args, sizeof(*param));
+	if (err)
+		goto free_param;
+	start = param->start;
+	if (param->end < param->start) {
+		err = -EINVAL;
+		goto free_param;
+	}
+	size = param->end - param->start;
+
+	image = fget(param->fd);
+	if (!image) {
+		pr_warn("pseudo_mm_misc driver recv invalid fd %d\n", param->fd);
+		err = -ENOENT;
+		goto free_param;
+	}
+	err = pseudo_mm_fill_anon_map(param->id, start, size, image,
+				      param->offset);
+
+	fput(image);
+
+free_param:
+	kfree(param);
+
+	return err ? err : 0;
+}
+
+static long pseudo_mm_unlocked_ioctl(struct file *filp, unsigned int cmd,
+				     unsigned long args)
+{
+	int pseudo_mm_id;
+	long err = 0;
+
+	// all ioctl in pseudo_mm need args
+	if (!args)
+		return -EINVAL;
+
+	switch (cmd) {
+	case PSEUDO_MM_IOC_CREATE:
+		// create a new pseudo_mm and return the id of it
+		pseudo_mm_id = create_pseudo_mm();
+		if (pseudo_mm_id < 0)
+			return pseudo_mm_id;
+		err = copy_to_user((int *)args, &pseudo_mm_id,
+				   sizeof(pseudo_mm_id));
+		if (err)
+			return err;
+		break;
+	case PSEUDO_MM_IOC_DELETE:
+		err = copy_from_user(&pseudo_mm_id, (int *)args,
+				     sizeof(pseudo_mm_id));
+		if (err)
+			return err;
+		delete_pseudo_mm(pseudo_mm_id);
+		break;
+	case PSEUDO_MM_IOC_ADD_ANON:
+		err = _pseudo_mm_add_anon((void *)args);
+		if (err)
+			return err;
+		break;
+	case PSEUDO_MM_IOC_FILL_ANON:
+		err = _pseudo_mm_fill_anon((void *)args);
+		if (err)
+			return err;
+		break;
+	default:
+		pr_warn("pseudo_mm_misc driver receive known cmd %u\n", cmd);
+		break;
+	}
+
+	return 0;
+}
+
+static int __init pseudo_mm_driver_init(void)
+{
+	int err;
+
+	err = misc_register(&pseudo_mm_device);
+	if (err) {
+		pr_err("pseudo_mm_misc driver register failed\n");
+		return err;
+	}
+
+	pr_info("pseudo_mm_misc driver register succeed !\n");
+	return 0;
+}
+
+static void __exit pseudo_mm_driver_exit(void)
+{
+	misc_deregister(&pseudo_mm_device);
+	pr_info("pseudo_mm_misc driver exit !\n");
+}
+
+module_init(pseudo_mm_driver_init);
+module_exit(pseudo_mm_driver_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("huang-jl <huangjl22@mails.tsinghua.edu.cn>");
+MODULE_DESCRIPTION("A driver used for exposing interface of pseudo_mm");
+MODULE_VERSION("0.1");
---
+++ drivers/misc/pseudo_mm/pseudo_mm_ioctl.h
@@ -0,0 +1,32 @@
+#ifndef __PSEUDO_MM_IOCTL_H__
+#define __PSEUDO_MM_IOCTL_H__
+
+#include <linux/ioctl.h>
+#include <linux/types.h>
+
+#define PSEUDO_MM_IOC_MAGIC 0x1c
+
+struct pseudo_mm_add_anon_param {
+	int id;
+	unsigned long start;
+	unsigned long end;
+	unsigned long prot;
+	unsigned long flags;
+};
+
+struct pseudo_mm_fill_anon_param {
+	int id;
+	unsigned long start;
+	unsigned long end;
+	int fd; // file descriptor of memory image file
+	off_t offset;	// offset within the image file
+};
+
+#define PSEUDO_MM_IOC_CREATE _IOR(PSEUDO_MM_IOC_MAGIC, 0x00, int *)
+#define PSEUDO_MM_IOC_DELETE _IOW(PSEUDO_MM_IOC_MAGIC, 0x01, int *)
+#define PSEUDO_MM_IOC_ADD_ANON \
+	_IOW(PSEUDO_MM_IOC_MAGIC, 0x02, struct pseudo_mm_add_anon_param *)
+#define PSEUDO_MM_IOC_FILL_ANON \
+	_IOW(PSEUDO_MM_IOC_MAGIC, 0x03, struct pseudo_mm_fill_anon_param *)
+
+#endif
